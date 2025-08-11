d040bc72ca1bff5e1659334712e4fd9b632889c0
Md-Moklesar-Rahman-Bappy <risingbappy1@gmail.com>
2025-08-11 11:24:37 +0600

    modified:   resources/views/layouts/navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -42,7 +42,7 @@
     <li class="nav-item dropdown no-arrow mx-1">
       <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown">
         <i class="fas fa-bell fa-fw"></i>
-        <span class="badge badge-danger badge-counter">3+</span>
+        <span class="badge badge-danger badge-counter">5+</span>
       </a>
       <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
         <h6 class="dropdown-header">Alerts Center</h6>
@@ -55,7 +55,7 @@
     <li class="nav-item dropdown no-arrow mx-1">
       <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown">
         <i class="fas fa-envelope fa-fw"></i>
-        <span class="badge badge-danger badge-counter">7</span>
+        <span class="badge badge-danger badge-counter">5+</span>
       </a>
       <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
         <h6 class="dropdown-header">Message Center</h6>
